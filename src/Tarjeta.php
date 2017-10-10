5284903a6b1671270288e8ef09710122f9aa34e0
Ciro Teves <32022610+LordRegginald@users.noreply.github.com>
2017-10-09 21:48:23 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,8 +3,10 @@
 namespace TpFinal;
 
 class Tarjeta {
-
+    public function pagar(Transporte $transporte, $fecha_y_hora);
+    public function recargar($monto);
     public function saldo() {
         return 0;
     }
+    public function viajesRealizados();
 }
