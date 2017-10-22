ec354bcd065919f6d63000121df1078be74203ab
Ciro Teves <32022610+LordRegginald@users.noreply.github.com>
2017-10-22 15:51:34 -0300

    Cambios mayores en Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,11 +2,39 @@
 
 namespace TpFinal;
 
+include 'Viaje.php';
+
 class Tarjeta {
-    public function pagar(Transporte $transporte, $fecha_y_hora);
-    public function recargar($monto);
-    public function saldo() {
+    protected $transporte, $fecha_y_hora, $monto, $saldoactual, $viajes_realizados;
+
+    public function pagar(Transporte $transporte, $fecha_y_hora) {
+        if('Colectivo' == get_tipo($transporte)){
+            
+        }
+        
+    };
+
+    public function recargar($monto) {
+        if($monto == 332) {
+            $this->saldoactual += 388;
+            print("Se acredito tu recarga de $" . $this->monto . " a la tarjeta más una suma de $56.\n");
+        }else{
+            if($monto == 500) {
+                $this->saldoactual += 652;
+                print("Se acredito tu recarga de $" . $this->monto . " a la tarjeta más una suma de $140.\n");
+            }
+        }else{
+            $this->saldoactual += $monto;
+            print("Se acredito tu recarga de $" . $this->monto . " a la tarjeta.\n");
+        }
         return 0;
     }
-    public function viajesRealizados();
+
+    public function saldo() {
+        print("Su saldo actual es de $" . $this->saldoactual . ".\n");
+    }
+
+    public function viajesRealizados() {
+        return $this->viajes_realizados;
+    };
 }
