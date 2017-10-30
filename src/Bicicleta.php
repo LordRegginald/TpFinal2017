73acf172f8b461b22945e718e6e9dd06c06b9f5e
Ciro Teves <32022610+LordRegginald@users.noreply.github.com>
2017-10-29 22:27:30 -0300

    Update Bicicleta.php

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -1,10 +1,21 @@
 <?php
 
-class Bicicleta {
-	public $boleto = 14.55;
-	public $patente;
+namespace TpFinal;
 
-	public function __construct( $patente ) {
-		$this->patente = $patente;
-	}
+class Bicicleta
+{
+    protected $tarifa_diaria = 14.55;
+    protected $patente;
+
+    public function __construct($patente) {
+        $this->patente = $patente;
+    }
+
+    public function get_unidad() {
+        return $this->patente;
+    }
+
+    public function get_boleto_bici() {
+        return $this->tarifa_diaria;
+    }
 }
