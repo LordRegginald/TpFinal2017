cd892a4b00af6dde87d9f9e14b26a50ed012e541
Ciro Teves <32022610+LordRegginald@users.noreply.github.com>
2017-10-31 01:15:18 -0300

    Agregados paréntesis a las funciones

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -11,19 +11,19 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->saldo(), 0);
     }
 
-    public function test1 {
+    public function test1() {
         $tarjeta = new Tarjeta(11111111);
         $tarjeta->recargar(50);
         $this->assertEquals($tarjeta->get_saldo(), 50.0);
     }
 
-    public function test2 {
+    public function test2() {
         $tarjeta = new Tarjeta(22222222);
         $tarjeta->recargar(332);
         $this->assertEquals($tarjeta->get_saldo(), 338.0);
     }
 
-    public function test3 {
+    public function test3() {
         $tarjeta = new Tarjeta(33333333);
         $tarjeta->recargar(332);
         $colectivo144Negro = new Colectivo("144 Negro", "Rosario Bus");
@@ -31,7 +31,7 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->get_saldo(), 328.3);
     }
 
-    public function test4 {
+    public function test4() {
         $tarjeta = new Tarjeta(44444444);
         $tarjeta->recargar(50);
         $colectivo135 = new Colectivo("135", "Rosario Bus");
@@ -40,7 +40,7 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->get_saldo(), 30.6);
     }
 
-    public function test5 {
+    public function test5() {
         $tarjeta = new Tarjeta(55555555);
         $tarjeta->recargar(50);
         $colectivo138 = new Colectivo("138", "Rosario Bus");
@@ -50,7 +50,7 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->get_saldo(), 37.66);
     }
 
-    public function test6 {
+    public function test6() {
         $tarjeta = new Tarjeta(66666666);
         $tarjeta->recargar(100);
         $colectivo138 = new Colectivo("138", "Rosario Bus");
@@ -61,7 +61,7 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->get_saldo(), 77.96);
     }
 
-    public function test7 {
+    public function test7() {
         $tarjeta = new Tarjeta(77777777);
         tarjeta->recargar(50);
         $bicicleta1234 = new Bicicleta("1234");
@@ -69,7 +69,7 @@ class EstacionTest extends TestCase
         $this->assertEquals($tarjeta->get_saldo(), 35.45);
     }
 
-    public function test8 {
+    public function test8() {
         $tarjeta = new Tarjeta(88888888);
         tarjeta->recargar(50);
         $bicicleta8008 = new Bicicleta("8008");
